357f2fbd561365ffcfe395dc5f1e103e19617563
Kevin <Kevin@wifi-130-215-221-81.dyn.wpi.edu>
2015-04-12 21:44:23 -0400

    more subclasses

--- GroupProject/src/sixesWild/Elimination.java
+++ GroupProject/src/sixesWild/Elimination.java
@@ -1,5 +1,16 @@
 package sixesWild;
 
 public class Elimination extends Level {
-
+	int movesRemaining;
+	
+	public Elimination(int movesRemaining)
+	{
+		super();
+		this.movesRemaining=movesRemaining;
+	}
+	
+	public boolean hasWon()
+	{
+		return false;
+	}
 }
--- GroupProject/src/sixesWild/Release.java
+++ GroupProject/src/sixesWild/Release.java
@@ -1,5 +1,16 @@
 package sixesWild;
 
 public class Release extends Level {
-
+	int movesRemaining;
+	
+	public Release(int movesRemaining)
+	{
+		super();
+		this.movesRemaining=movesRemaining;
+	}
+	
+	public boolean hasWon()
+	{
+		return false;
+	}
 }
