5794ad48edd6dd808d2884ff32a7c4c9e08ee10c
Kevin <Kevin@autoreg-664948.wifi.wpi.edu>
2015-04-14 19:59:16 -0400

    splash test

--- GroupProject/test/sixesWildBoundary/TestSplashScreen.java
+++ GroupProject/test/sixesWildBoundary/TestSplashScreen.java
@@ -3,5 +3,23 @@ package sixesWildBoundary;
 import junit.framework.TestCase;
 
 public class TestSplashScreen extends TestCase {
-
+	SplashScreenGame sg;
+	
+	protected void setUp()
+	{
+		sg=new SplashScreenGame(1000);
+		sg.showSplash();
+		sg.setVisible(true);
+	}
+	
+	protected void tearDown(){
+		if(sg != null){
+			sg.dispose() ;
+		}
+	}
+	
+	public void testSplashScreenGame()
+	{
+		System.out.println("Sample Splash Screen");
+	}
 }
