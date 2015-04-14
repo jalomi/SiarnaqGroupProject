813befa623b8a3fe19c4e70d83010dac9b05f261
Kevin <Kevin@autoreg-664948.wifi.wpi.edu>
2015-04-14 19:53:13 -0400

    sixes wild test

--- GroupProject/test/sixesWildBoundary/TestSixesWildApplication.java
+++ GroupProject/test/sixesWildBoundary/TestSixesWildApplication.java
@@ -1,7 +1,20 @@
 package sixesWildBoundary;
 
+import sixesWildEntity.Level;
 import junit.framework.TestCase;
 
 public class TestSixesWildApplication extends TestCase {
-
+	SixesWildApplication s;
+	Level l;
+	
+	protected void setUp()
+	{
+		l=new Level();
+		s=new SixesWildApplication(l);
+	}
+	
+	public void testSixesWildApplication()
+	{
+		System.out.println("Sample Sixes Wild");
+	}
 }
--- GroupProject/test/sixesWildBoundary/TestSixesWildLevelPanel.java
+++
@@ -1,7 +0,0 @@
-package sixesWildBoundary;
-
-import junit.framework.TestCase;
-
-public class TestSixesWildLevelPanel extends TestCase {
-
-}
