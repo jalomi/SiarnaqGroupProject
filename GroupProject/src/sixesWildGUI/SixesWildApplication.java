1a7ff32d61364e2a8ac326669af07a0a8a587542
jalomi <jalomi@wpi.edu>
2015-04-13 20:47:52 -0400

    All frames non-resizeable and titles added.  For real this time

--- GroupProject/src/sixesWildGUI/SixesWildApplication.java
+++ GroupProject/src/sixesWildGUI/SixesWildApplication.java
@@ -38,6 +38,8 @@ public class SixesWildApplication extends JFrame {
 	 * Create the frame.
 	 */
 	public SixesWildApplication() {
+		setResizable(false);
+		setTitle("Sixes Wild");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 580, 750);
 		contentPane = new JPanel();
