a30bbd77df126b75641c5b92abc544819e5c2168
jalomi <jalomi@wpi.edu>
2015-04-14 10:13:59 -0400

    Added image to the game panel.  NOTE: will only show up when on my PC because it is a local file.  I'm working on adding the file to the project resources

--- GroupProject/src/sixesWildBoundary/SixesWildGamePanel.java
+++ GroupProject/src/sixesWildBoundary/SixesWildGamePanel.java
@@ -2,6 +2,7 @@ package sixesWildBoundary;
 
 import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -439,21 +440,26 @@ public class SixesWildGamePanel extends JPanel {
 		this.setLayout(new GridLayout(9,9));
 		this.setBorder(new EmptyBorder(10,10,10,10));
 		
-		this.level = Level.newInstance(1, 0.2, 0.1, 0.3, .15, 0.1, 0.15);
-		this.board = new Board(level);
+		
+		//unused for group phase1		
+		//this.level = Level.newInstance(1, 0.2, 0.1, 0.3, .15, 0.1, 0.15);
+		//this.board = new Board(level);
+		
+		//TEMPORARY for group phase1
+		JLabel[][] map = new JLabel[9][9] ;
 		
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
-				/*
-				 * Here is the place to add image
-				 * 
-				 * 
-				 */
-				Tile[][] map = board.getMap();
+				//TEMPORARY for group phase1
+				
 				//you may want to use if statement to decide which image to feed
 				//int value = map[i][j].getSqaure().getValue();
-				JLabel b = new JLabel("Test");
-				this.add(b);
+				//JLabel b = new JLabel("Test");
+				//this.add(b);
+				
+				map[i][j] = new JLabel("") ;
+				map[i][j].setIcon(new ImageIcon("C:\\Users\\John\\Documents\\WPI\\2-Sophomore\\Software Engineering\\Group Project\\Tile Images\\1-1.png"));
+				this.add(map[i][j]) ;
 			}
 		}
 		this.validate();
