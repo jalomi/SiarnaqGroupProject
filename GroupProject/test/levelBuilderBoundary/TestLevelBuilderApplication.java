d5931bda17e92cdda7c1310e3a1686eaeeea9508
kyleyoung14 <kcyoung@wpi.edu>
2015-04-14 19:16:06 -0400

    TestLevelBuilderApplication complete

--- GroupProject/test/levelBuilderBoundary/TestLevelBuilderApplication.java
+++ GroupProject/test/levelBuilderBoundary/TestLevelBuilderApplication.java
@@ -3,5 +3,24 @@ package levelBuilderBoundary;
 import junit.framework.TestCase;
 
 public class TestLevelBuilderApplication extends TestCase {
-
+	
+	LevelBuilderApplication LBA ;
+	
+	@Override
+	protected void setUp(){
+		LBA = new LevelBuilderApplication() ;
+		
+		LBA.setVisible(true) ;
+	}
+	
+	@Override
+	protected void tearDown(){
+		if(LBA != null){
+			LBA.dispose() ;
+		}
+	}
+	
+	public void test(){
+		 System.out.println("Sample LevelBuilder GUI") ;
+	}
 }
