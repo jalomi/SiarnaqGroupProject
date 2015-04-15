d0b34456980a488cf226c9c153fc91dd0e963c1c
jalomi <jalomi@wpi.edu>
2015-04-14 20:03:52 -0400

    added test case for other splash screen

--- GroupProject/test/sixesWildBoundary/TestSplashScreen.java
+++ GroupProject/test/sixesWildBoundary/TestSplashScreen.java
@@ -1,7 +1,28 @@
 package sixesWildBoundary;
 
 import junit.framework.TestCase;
+import sixesWildBoundary.SplashScreenGame;
 
 public class TestSplashScreen extends TestCase {
+	
+	SplashScreenGame splash ;
+	
+	@Override
+	protected void setUp(){
+		splash = new SplashScreenGame(1000) ;
+		splash.showSplash();
+		splash.setVisible(true) ;
+	}
+	
+	@Override
+	protected void tearDown(){
+		if(splash != null){
+			splash.dispose() ;
+		}
+	}
+	
+	public void test(){
+		 System.out.println("Sample SplashScreen") ;
+	}
 
 }
