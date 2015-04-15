56d5b0cfdbff9a3f789927a3866470fcd5accefb
Kevin <Kevin@autoreg-664948.wifi.wpi.edu>
2015-04-14 20:01:17 -0400

    main menu test

--- GroupProject/test/sixesWildBoundary/TestMainMenuApplication.java
+++ GroupProject/test/sixesWildBoundary/TestMainMenuApplication.java
@@ -3,5 +3,15 @@ package sixesWildBoundary;
 import junit.framework.TestCase;
 
 public class TestMainMenuApplication extends TestCase {
-
+	MainMenuApplication m;
+	
+	protected void setUp()
+	{
+		m=new MainMenuApplication();
+	}
+	
+	public void testMainMenu()
+	{
+		System.out.println("Sample Main Menu");
+	}
 }
