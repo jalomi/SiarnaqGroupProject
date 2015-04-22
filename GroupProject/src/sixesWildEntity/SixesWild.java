8fa1e332fbfb820f58b06c8f85b851fec997a85b
jalomi <jalomi@wpi.edu>
2015-04-22 12:41:16 -0400

    added initialize stuff

--- GroupProject/src/sixesWildEntity/SixesWild.java
+++ GroupProject/src/sixesWildEntity/SixesWild.java
@@ -3,29 +3,58 @@ package sixesWildEntity;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import sixesWildBoundary.SixesWildApplication;
+
 public class SixesWild implements Iterable<Level>{
 	private ArrayList<Level> levels;
 	private Board board;
+	private SixesWildApplication app ;
 	
 	public SixesWild(ArrayList<Level> l, Board b)
 	{
-		this.levels=l;
-		this.board=b;
+		this.levels = l;
+		this.board = b;
 	}
 
 	@Override
 	public Iterator<Level> iterator() {
 		// TODO Auto-generated method stub
+		
+		//don't know if this is how we should do it:
+		//SixesWildApplication app = new SixesWildApplication(Level) ;
+		
 		return null;
 	}
 	
-	public ArrayList<Level> getLevels()
-	{
+	
+	public void intialize(){
+		//initialize the game
+		initializeModel() ;
+		initializeView() ;
+		initializeControllers() ;
+	}
+	
+	
+	private void initializeModel() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void initializeView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initializeControllers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<Level> getLevels(){
 		return levels;
 	}
 	
-	public Board getBoard()
-	{
+	public Board getBoard(){
 		return board;
 	}
 	
