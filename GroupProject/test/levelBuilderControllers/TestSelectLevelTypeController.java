ec9ee1c6158ea1262bd614fbb94134fed29540c8
Kevin <Kevin@wifi-130-215-217-184.dyn.wpi.edu>
2015-05-02 01:42:41 -0400

    level type and tile controller tests

---
+++ GroupProject/test/levelBuilderControllers/TestSelectLevelTypeController.java
@@ -0,0 +1,48 @@
+package levelBuilderControllers;
+
+import java.io.IOException;
+
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+
+import junit.framework.TestCase;
+import levelBuilderBoundary.LevelBuilderApplication;
+
+import org.json.JSONException;
+
+public class TestSelectLevelTypeController extends TestCase{
+LevelBuilderApplication a;
+	
+	@Override
+	protected void setUp() throws IOException, JSONException {
+		a = new LevelBuilderApplication();
+		
+		a.setVisible(true);
+	}
+	
+	@Override
+	protected void tearDown() {
+		a.dispose();
+	}
+	
+	public void testLightning() throws IOException, JSONException
+	{
+		SelectLevelTypeController sltc=new SelectLevelTypeController(LevelBuilderApplication.model, a);
+		JComboBox cb=a.getInputPanel().getType();
+		cb.setSelectedItem("Lightning");
+		sltc.update(cb);
+		
+		assertEquals(LevelBuilderApplication.model.getLevelType(), "Lightning");
+	}
+	
+	public void testRelease() throws IOException, JSONException
+	{
+		SelectLevelTypeController sltc=new SelectLevelTypeController(LevelBuilderApplication.model, a);
+		JComboBox cb=a.getInputPanel().getType();
+		cb.setSelectedItem("Release");
+		sltc.update(cb);
+		
+		assertEquals(LevelBuilderApplication.model.getLevelType(), "Release");
+		assertEquals(LevelBuilderApplication.model.getTileActiveAt(8, 2), false);
+	}
+}
---
+++ GroupProject/test/levelBuilderControllers/TestSelectTilesController.java
@@ -0,0 +1,46 @@
+package levelBuilderControllers;
+
+import java.io.IOException;
+
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+
+import junit.framework.TestCase;
+import levelBuilderBoundary.LevelBuilderApplication;
+
+import org.json.JSONException;
+
+public class TestSelectTilesController extends TestCase{
+LevelBuilderApplication a;
+	
+	@Override
+	protected void setUp() throws IOException, JSONException {
+		a = new LevelBuilderApplication();
+		
+		a.setVisible(true);
+	}
+	
+	@Override
+	protected void tearDown() {
+		a.dispose();
+	}
+	
+	public void testSelect() throws IOException, JSONException
+	{
+		JToggleButton tb=a.getGamePanel().getTile(5, 2);
+		SelectTilesController stc=new SelectTilesController(LevelBuilderApplication.model, a, tb, 5, 2);
+		stc.process();
+		
+		assertEquals(LevelBuilderApplication.model.getTileActiveAt(5, 2), true);
+	}
+	
+	public void testDeselect() throws IOException, JSONException
+	{
+		JToggleButton tb=a.getGamePanel().getTile(5, 2);
+		SelectTilesController stc=new SelectTilesController(LevelBuilderApplication.model, a, tb, 5, 2);
+		tb.setSelected(true);
+		stc.process();
+		
+		assertEquals(LevelBuilderApplication.model.getTileActiveAt(5, 2), false);
+	}
+}
