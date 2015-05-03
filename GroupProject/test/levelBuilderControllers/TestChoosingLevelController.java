1617ac77cefb3f2d0aae8ead3cb16dbfbbcc9bcc
Kevin <Kevin@wifi-130-215-217-184.dyn.wpi.edu>
2015-05-03 17:28:37 -0400

    Choose Level test updated

--- GroupProject/test/levelBuilderControllers/TestChoosingLevelController.java
+++ GroupProject/test/levelBuilderControllers/TestChoosingLevelController.java
@@ -20,10 +20,43 @@ LevelBuilderApplication a;
 		a.dispose();
 	}
 	
-	public void testChooseLevel() throws IOException, JSONException
+	public void testPuzzle() throws IOException, JSONException
 	{
 		ChoosingLevelController clc=new ChoosingLevelController(a);
 		a.getInputPanel().getChoosingLevel().setSelectedItem("Puzzle 1");
 		clc.update(a.getInputPanel().getChoosingLevel());
+		assertEquals(a.model.getLevelNumber(), 1);
+	}
+	
+	public void testLightning() throws IOException, JSONException
+	{
+		ChoosingLevelController clc=new ChoosingLevelController(a);
+		a.getInputPanel().getChoosingLevel().setSelectedItem("Lightning 2");
+		clc.update(a.getInputPanel().getChoosingLevel());
+		assertEquals(a.model.getLevelNumber(), 2);
+	}
+	
+	public void testElimination() throws IOException, JSONException
+	{
+		ChoosingLevelController clc=new ChoosingLevelController(a);
+		a.getInputPanel().getChoosingLevel().setSelectedItem("Elimination 3");
+		clc.update(a.getInputPanel().getChoosingLevel());
+		assertEquals(a.model.getLevelNumber(), 3);
+	}
+	
+	public void testRelease() throws IOException, JSONException
+	{
+		ChoosingLevelController clc=new ChoosingLevelController(a);
+		a.getInputPanel().getChoosingLevel().setSelectedItem("Release 4");
+		clc.update(a.getInputPanel().getChoosingLevel());
+		assertEquals(a.model.getLevelNumber(), 4);
+	}
+	
+	public void testNewLevel() throws IOException, JSONException
+	{
+		ChoosingLevelController clc=new ChoosingLevelController(a);
+		a.getInputPanel().getChoosingLevel().setSelectedItem("New Level");
+		clc.update(a.getInputPanel().getChoosingLevel());
+		//assertEquals(a.model.getLevelNumber(), 0);
 	}
 }
