06032adc58a517b42d9d5e4f6391e70ab5538811
jalomi <jalomi@wpi.edu>
2015-05-03 17:20:09 -0400

    documentation

--- GroupProject/src/sixesWildControllers/ResetProgressController.java
+++ GroupProject/src/sixesWildControllers/ResetProgressController.java
@@ -7,15 +7,26 @@ import javax.swing.JFrame;
 
 import sixesWildEntity.SixesWild;
 
+/**
+ * Will reset the progress of the levels
+ * @author John
+ *
+ */
 public class ResetProgressController implements ActionListener{
-
+	/** the frame that initiated the controller */
 	JFrame frame ;
 	
+	/**
+	 * Constructor
+	 * @param f
+	 */
 	public  ResetProgressController(JFrame f){
 		this.frame = f ;
 	}
 	
-	
+	/**
+	 * Resets the level progress
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		SixesWild sw  = SixesWild.newInstance();
